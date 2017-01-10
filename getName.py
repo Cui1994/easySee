54fbd05054e4d6bbb9b252d8f6d242fa7248492c
Cui1994 <269584357@qq.com>
2017-01-10 17:45:33 +0800

    get_anchor_name

---
+++ getName.py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*-
+
+import requests, json, re
+
+class Anchor:
+	tv = ''
+	room = ''
+	is_live = False
+	name = None
+
+anchor = Anchor()
+anchor.tv = str(input('enter tv:'))
+anchor.room = str(input('enter room:'))
+
+class NameGetter:
+
+	def __init__(self, anchor):
+		if anchor.tv == 'douyu':
+			self.douyu(anchor)
+		elif anchor.tv == 'xiongmao':
+			self.xiongmao(anchor)
+		elif anchor.tv == 'longzhu':
+			self.longzhu(anchor)
+		elif anchor.tv == 'quanmin':
+			self.quanmin(anchor)
+		elif anchor.tv == 'huya':
+			self.huya(anchor)
+		elif anchor.tv == 'zhanqi':
+			self.zhanqi(anchor)
+		else:
+			return None
+
+	def douyu(self, anchor):
+		try:
+			url = 'https://www.douyu.com/' + anchor.room
+			r = requests.get(url).text
+			pattern = re.compile(r'class="zb-name">(.*?)</a>', re.S)
+			anchor.name = re.findall(pattern, r)[0]
+		except:
+			return None
+
+	def xiongmao(self, anchor):
+		try:
+			url = 'http://www.panda.tv/' + anchor.room
+			r = requests.get(url).text
+			pattern = re.compile(r'<title>(.*?)</title>', re.S)
+			anchor.name = re.findall(pattern, r)[0][:-16]
+		except:
+			return None
+
+	def longzhu(self, anchor):
+		try:
+			url = 'http://star.longzhu.com/' + anchor.room
+			r = requests.get(url).text
+			pattern = re.compile(r'class="header-info-name">(.*?)</span>', re.S)
+			anchor.name = re.findall(pattern, r)[0]
+		except:
+			return None
+
+	def quanmin(self, anchor):
+		try:
+			url = 'http://www.quanmin.tv/json/rooms/' + anchor.room + '/noinfo4.json'
+			r = requests.get(url).json()
+			anchor.name = r.get("nick")
+		except:
+			return None
+
+	def huya(self, anchor):
+		url = 'http://www.huya.com/' + anchor.room
+		r = requests.get(url).text
+		pattern = re.compile(r' <span class="host-name" title="(.*?)">', re.S)
+		anchor.name = re.findall(pattern, r)[0]
+
+	def zhanqi(self, anchor):
+		try:
+			url = 'https://www.zhanqi.tv/' + anchor.room
+			r = requests.get(url).text
+			pattern = re.compile(r'<p class="name dv">(.*?)</p>', re.S)
+			anchor.name = re.findall(pattern, r)[0]
+		except:
+			return None
+
+NameGetter(anchor)
+print anchor.name
